39947cfba97f7350233a953731184cf11c4f5394
Brooke Bratcher <brookebratcher@Brookes-MacBook-Air.local>
2026-01-01 17:24:51 -0600

    Bump version to 1.0.2

--- version.py
+++ version.py
@@ -1,7 +1,7 @@
 """Version information for Trail Camera Software."""
 
-__version__ = "1.0.1"
-__version_info__ = (1, 0, 1)
+__version__ = "1.0.2"
+__version_info__ = (1, 0, 2)
 
 # GitHub repository for updates
 GITHUB_REPO = "cabbp3/Trail-Camera-Software"
