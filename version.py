f5c66f232330e2e2e3546ea5f24fe3284e275fa6
Brooke Bratcher <brookebratcher@Brookes-MacBook-Air.local>
2026-02-22 10:03:47 -0600

    Disable PYZ compression and UPX for Windows build

--- version.py
+++ version.py
@@ -1,7 +1,7 @@
 """Version information for Trail Camera Software."""
 
-__version__ = "1.1.3"
-__version_info__ = (1, 1, 3)
+__version__ = "1.1.4"
+__version_info__ = (1, 1, 4)
 
 # GitHub repository for updates
 GITHUB_REPO = "cabbp3/Trail-Camera-Software"
